8f6f34aa2d353398fe487ea527317fbc62ddea70
Jakey <jp@jakelprice.com>
2021-12-22 12:23:38 +0000

    Remove overflow hidden from form section

--- src/Resources/Design/Section/two-col.blade.php
+++ src/Resources/Design/Section/two-col.blade.php
@@ -11,7 +11,7 @@
     </div>
     <div class="md:col-span-2 space-y-8">
         
-        <div class="bg-white rounded-md overflow-hidden">
+        <div class="bg-white rounded-md">
             <div class="lg:hidden bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
                 <h3 class="text-lg leading-6 font-medium text-gray-900">
                     {!! $title !!}
