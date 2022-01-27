46af0c1faa17395e126080a757f653b3a76bc79c
Jakey <jp@jakelprice.com>
2022-01-27 08:15:30 +0000

    Added one-col to sections

---
+++ src/Resources/Design/Section/one-col.blade.php
@@ -0,0 +1,13 @@
+<div class="bg-white shadow rounded-md">
+  <div class="bg-white border-b border-gray-200 p-6">
+    <h3 class="text-lg leading-6 font-medium text-gray-900">
+      {!! $title !!}
+    </h3>
+    <p class="mt-1 text-sm text-gray-500">
+      {!! $description !!}
+    </p>
+  </div>
+  <div>
+    {!! $slot !!}
+  </div>
+</div>
