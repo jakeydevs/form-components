4280d1c99eced2c92bc31af0e241661e308fd3b6
Jakey <jp@jakelprice.com>
2022-02-15 16:34:06 +0000

    Better upload image style

--- src/Resources/upload.blade.php
+++ src/Resources/upload.blade.php
@@ -24,20 +24,33 @@ text="{{ $label }}"
 />
 @endif
 
-<div class="flex justify-start">
+<div class="flex justify-start items-center space-x-3">
   @if (!is_null($getValue()))
   <div class="shrink-0">
-  @switch($getFileType())
-
+    @switch($getFileType())
+    
     @case('FILE')
-    FILE
-    @break
 
-    @case("IMAGE")
-    IMAGE
+    @php
+      $ext = pathinfo($getValue(), PATHINFO_EXTENSION);
+    @endphp
+    <a href='{{ $getValue() }}' target="_blank">
+      <div class="text-center text-gray-600 hover:text-blue-500">
+        <div>
+          <svg class="w-12 h-12" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"></path></svg>
+        </div>
+        <div class="uppercase text-sm">{{ $ext }}</div>
+      </div>
+    </a>
     @break
     
-  @endswitch
+    @case("IMAGE")
+    <a href='{{ $getValue() }}' target="_blank">
+      <img src='{{ $getValue() }}' class='h-14 object-cover rounded' />
+    </a>
+    @break
+    
+    @endswitch
   </div>
   @endif
   <input 
